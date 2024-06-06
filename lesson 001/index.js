3f3bc19db3bf5552a016c46a00183955ef6e295b
Matyuscha <matymatik007@gmail.com>
2024-06-06 13:53:43 +0300

    index.js

--- lesson 001/index.js
+++ lesson 001/index.js
@@ -2,49 +2,130 @@ console.log('Lesson 001');
 
 (() => {
     // place your code here for paragraph 2.4 (https://uk.javascript.info/variables)
-
-
-    
+    // 1
+    // let admin, name;
+    // name = "Іван";
+    // admin = name;
+    // alert(admim);    
+    // 2
+    // let planet;
+    // planet = "Земля";
+    // let UserName = "Матвій";
+    // 3 дата в верхньому реєстрі, age у нижньому
 })();
 
 (() => {
     // place your code here for paragraph 2.5 (https://uk.javascript.info/types)
-
-
-
+    // привіт 1
+    // привіт name
+    // привіт Ілля
 })();
 
 (() => {
     // place your code here for paragraph 2.6 (https://uk.javascript.info/alert-prompt-confirm)
-
-
-
+    // let yourname = prompt("Як вас звати?");
+    // alert(yourname)
 })();
 
 (() => {
     // place your code here for paragraph 2.8 (https://uk.javascript.info/operators)
-
-
-
+    // 1. 
+    // let a = 1, b = 1;
+    // let c = ++a;
+    // let d = b++;
+    // alert( ++a ); 
+    // alert( b++ );
+    // alert( a ); 
+    // alert( b );
+    // 2. a = 4, x = 5
+    // 3. 
+    // "10"
+    // -1
+    // 1
+    // 2
+    // 6
+    // "9px"
+    // "$45"
+    // 2
+    // NaN
+    // 4
+    // -14
+    // 1
+    // NaN
+    // -2
+    // 3.
+    // let a = prompt("Перше число?", 1);
+    // let b = prompt("Друге число?", 2);
+    // alert(+a + +b);
 })();
 
 (() => {
     // place your code here for paragraph 2.9 (https://uk.javascript.info/comparison)
-
-
-
+    //1.
+    // true
+    // false
+    // true
+    // true
+    // false
+    // false
+    // false
 })();
 
 (() => {
     // place your code here for paragraph 2.10 (https://uk.javascript.info/ifelse)
+    // 1. yes
+    // 2.
+    // let answer = prompt('Яка "офіційна" назва JavaScript?', '');
 
-
-
+    // if (answer == 'ECMAScript') {
+    //   alert('Правильно!');
+    // } else {
+    //   alert('Ви не знаєте? ECMAScript!');
+    // }
+    // 3.
+    //let answer = prompt("Введіть число:");
+    //if (answer > 0) {
+        //alert(1);
+        //} else if (answer < 0) {
+        //alert(-1);
+        //} else {
+        //alert(0);
+    //}
+    // 4.
+    // let result = (a + b < 4) ? 'Нижче' : 'Вище';
+    //5. 
+    // let message = (login == 'Працівник') ? 'Привіт' :
+    //     (login == 'Директор') ? 'Вітаю' :
+    //     (login == '') ? 'Немає логіну' :
+    //     '';
 })();
 
 (() => {
     // place your code here for paragraph 2.11 (https://uk.javascript.info/logical-operators)
-// 1. false
-
-
+    // 1. 2
+    // 2.  спочатку 1 потім 2
+    // 3. null
+    // 4. спочатку 1 потім undefined
+    // 5. 3
+    // 6. if (age >= 14 && age <= 90)
+    // 7.
+    // if (!(age >= 14 && age <= 90))
+    // if (age < 14 || age > 90)
+    // 8. перший і третій
+    // 9.
+    // let userName = prompt('Хто там?', '');
+    // if (userName === 'Admin') {
+    //     let pass = prompt('Пароль?', '');
+    //      if (pass === 'Господар') {
+    //         alert( 'Ласкаво просимо!' );
+    //     } else if (pass === '' || pass === null) {
+    //         alert( 'Скасовано' );
+    //      } else {
+    //         alert( 'Неправильний пароль' );
+    //     }
+    // } else if (userName === '' || userName === null) {
+    //     alert( 'Скасовано' );
+    // } else {
+    //     alert( 'Я вас не знаю' );
+    // }
 })();
