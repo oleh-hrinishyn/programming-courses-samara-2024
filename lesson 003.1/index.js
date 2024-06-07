f27227086c8012405aa7efc314ccbac9aa042611
oleh-hrinishyn <ogrinishin+1@gmail.com>
2024-06-07 16:12:25 +0300

    task 3

---
+++ lesson 003.1/index.js
@@ -0,0 +1,6 @@
+// Write a recursive function to check if a string is a palindrome.
+// console.log(isPalindrome("racecar")); // Output: true
+
+
+// Write a recursive function to compute the power of a number.
+// console.log(power(2, 3)); // Output: 8
