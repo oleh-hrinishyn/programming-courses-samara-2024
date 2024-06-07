05b78deb5dc340acc8d866de5e06a069181e9b5a
oleh-hrinishyn <ogrinishin+1@gmail.com>
2024-06-07 16:06:12 +0300

    task 3

---
+++ lesson 003.1/index.js
@@ -0,0 +1,7 @@
+//Write a recursive function to calculate the factorial of a number.
+//console.log(factorial(5)); // Output: 120
+
+
+
+// Write a recursive function to generate all permutations of a string.
+//console.log(permute("abc")); // Output: ["abc", "acb", "bac", "bca", "cab", "cba"]
