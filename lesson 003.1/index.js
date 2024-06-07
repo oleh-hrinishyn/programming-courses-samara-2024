3bf8dc4758619a474a16538ee729732e2343eade
oleh-hrinishyn <ogrinishin+1@gmail.com>
2024-06-07 16:09:12 +0300

    task 3

---
+++ lesson 003.1/index.js
@@ -0,0 +1,6 @@
+// Write a recursive function to find the sum of an array of integers.
+// console.log(sumArray([1, 2, 3, 4, 5])); // Output: 15
+
+
+// Write a recursive function to flatten a nested array.
+// console.log(flattenArray([1, [2, [3, 4], 5], 6])); // Output: [1, 2, 3, 4, 5, 6]
