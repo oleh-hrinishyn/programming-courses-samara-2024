b8463a24f6a9251fae6bcaeac4d34d01bc51e536
oleh-hrinishyn <ogrinishin+1@gmail.com>
2024-06-07 16:07:34 +0300

    task 3

---
+++ lesson 003.1/index.js
@@ -0,0 +1,5 @@
+// Write a recursive function to generate the nth Fibonacci number.
+// console.log(fibonacci(6)); // Output: 8
+
+// Write a recursive function to count the occurrences of a value in an array.
+// console.log(countOccurrences([1, 2, 3, 4, 2, 2, 5], 2)); // Output: 3
