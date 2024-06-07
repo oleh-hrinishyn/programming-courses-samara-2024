03b9dde3ff5f8f3ecdddc296bbe9765aec5aab33
oleh-hrinishyn <ogrinishin+1@gmail.com>
2024-06-07 16:10:42 +0300

    task 3

---
+++ lesson 003.1/index.js
@@ -0,0 +1,6 @@
+// Write a recursive function to find the GCD of two numbers.
+// console.log(gcd(48, 18)); // Output: 6
+
+
+// Write a recursive function to reverse a string.
+// console.log(reverseString("hello")); // Output: "olleh"
