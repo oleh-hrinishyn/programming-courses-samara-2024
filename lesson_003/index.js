34c4ddd18d99e75c65a5a21e04f928f7f2780e40
Danylo <danillitvin531@gmail.com>
2024-06-09 20:59:08 +0300

    lesson 003 commit

---
+++ lesson_003/index.js
@@ -0,0 +1,66 @@
+console.log('Lesson 001');
+
+(() => {
+    // place your code here for paragraph 2.15
+    //function checkAge1(age) {
+        //return (age > 18) ? true : confirm("Батьки дозволили?")
+    //}
+
+    //function checkAge2(age) {
+        //return (age > 18) || confirm("Батьки дозволили?")
+    //}
+
+    //function min(a, b) {
+        //if (a < b) {
+            //return a;
+        //} else {
+            //return b;
+        //}
+    //}
+
+    //or
+
+    //function min(a, b) {
+        //return a < b ? a : b;
+    //}
+
+    //console.log(min(2, 5))
+    //console.log(min(3, -1))
+    //console.log(min(1, 1))
+
+    //function pow(x, n) {
+        //if (n < 1){
+            //return false;
+        //} else if (n == 1) {
+            //return x;
+        //} else {
+            //return x * pow(x, n - 1);
+        //}
+    //}
+    
+    //console.log(pow(3, 2))
+    //console.log(pow(3, 3))
+    //console.log(pow(1, 100))
+
+    
+})();
+
+(() => {
+    // place your code here for paragraph 2.16
+    //...
+
+
+})();
+
+(() => {
+    // place your code here for paragraph 2.17
+    //let ask = (question, yes, no) =>  confirm(question) ? yes() : no()
+
+    //ask(
+        //"Ви згодні?",
+        //function() { alert("Ви погодились."); },
+        //function() { alert("Ви скасували виконання."); }
+      //);
+
+
+})();
