e77d06872915fe5a1b27d8401d2d47d03392de21
Pyreshka <twinkkus@gmail.com>
2024-06-09 16:52:56 +0300

    3rd task

---
+++ lesson 003/index.js
@@ -0,0 +1,50 @@
+console.log('Lesson 002');
+
+(() => {
+    // place your code here for paragraph 2.15
+
+})();
+
+
+(() => {
+    // place your code here for paragraph 2.16
+
+
+})();
+
+(() => {
+    // place your code here for paragraph 2.17
+// 1. 
+// немає різниці
+
+// 2.
+// function checkAge(age){
+//     return(age>18)? true : confirm("Батьки дозволили?")
+// }
+// function checkAge(age) {
+//     return (age > 18) || confirm("Батьки дозволили?")
+// }
+
+// 3.
+// function min(a,b){
+//     return a<b ? a :b
+// }
+
+// 4.
+// function pow(x,n){
+//     if (n>0){
+//         for (i=1; i<n; i++){
+//             x *= x
+//         }
+//     return x 
+//     }
+// }
+// let x = prompt("x=")
+// let n = prompt("n=")
+// if (n<1){
+//     alert("оберіть натуральну степінь")
+// }
+// else{
+//     alert(pow(x,n))
+// }
+})();
