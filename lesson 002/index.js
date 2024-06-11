757ef51db624f47f727e886ff2e876d136f6589d
DavGriTo <davgri184@gmail.com>
2024-06-11 09:51:30 +0300

    update main 002

---
+++ lesson 002/index.js
@@ -0,0 +1,11 @@
+console.log('Lesson 002');
+
+(() => {
+    // place your code here for paragraph 2.13 (https://uk.javascript.info/variables)
+
+})();
+
+(() => {
+    // place your code here for paragraph 2.14 (https://uk.javascript.info/logical-operators)
+
+})();
