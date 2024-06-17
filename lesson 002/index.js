2202a88c02578808209ae50bdc9434aaaed1ea80
Ed2105 <eduard.igtmnanu@gmail.com>
2024-06-17 11:07:43 +0300

    my home task for 001 and 002

---
+++ lesson 002/index.js
@@ -0,0 +1,50 @@
+console.log('Lesson 001');
+
+(() => {
+    // place your code here for paragraph 2.4 (https://uk.javascript.info/variables)
+
+
+    
+})();
+
+(() => {
+    // place your code here for paragraph 2.5 (https://uk.javascript.info/types)
+
+
+
+})();
+
+(() => {
+    // place your code here for paragraph 2.6 (https://uk.javascript.info/alert-prompt-confirm)
+
+
+
+})();
+
+(() => {
+    // place your code here for paragraph 2.8 (https://uk.javascript.info/operators)
+
+
+
+})();
+
+(() => {
+    // place your code here for paragraph 2.9 (https://uk.javascript.info/comparison)
+
+
+
+})();
+
+(() => {
+    // place your code here for paragraph 2.10 (https://uk.javascript.info/ifelse)
+
+
+
+})();
+
+(() => {
+    // place your code here for paragraph 2.11 (https://uk.javascript.info/logical-operators)
+// 1. false
+
+
+})();
